0a961b59f83c629dc56a8903a30f9202aa3c3794
Vynrae06 <ghaith.elghaddaf@gmail.com>
2024-02-01 21:29:15 +0100

    Two Player Controls
    Was done with the Input.joy functions

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var PLAYER_INDEX: int
 @export var MOVEMENT_SPEED: float = 666
 @export var MOVEMENT_SPEED_LEFT: float = 250
 
@@ -10,26 +11,32 @@ func _ready():
 	$AnimatedSprite2D.play("idle")
 
 func _process(_delta):
-	shoot()
+	shoot_basic()
 
 func _physics_process(_delta):
 	move()
 	move_and_slide()
 
 func move():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_x = Input.get_joy_axis(PLAYER_INDEX, JOY_AXIS_LEFT_X)
+	var input_y = Input.get_joy_axis(PLAYER_INDEX, JOY_AXIS_LEFT_Y)
+	var input_direction = Vector2(input_x, input_y)
 	velocity = input_direction * MOVEMENT_SPEED
 
-func shoot():
-	if Input.is_action_pressed("shoot_normal") and CAN_SHOOT: 
+func shoot_basic():
+	if Input.is_joy_button_pressed(PLAYER_INDEX, JOY_BUTTON_B) and CAN_SHOOT: 
 		var spawned_projectile = PROJECTILE_BASIC_SCENE.instantiate() as Area2D
 		spawned_projectile.global_position = $ShotInstantiateMarker.global_position
 		$"../ProjectilesHolder".add_child(spawned_projectile)
 		$ShotTimer.start()
 		CAN_SHOOT = false
 
+func shoot_special():
+	if Input.is_joy_button_pressed(PLAYER_INDEX, JOY_BUTTON_RIGHT_SHOULDER):
+		pass
+
 func _on_shot_timer_timeout():
 	CAN_SHOOT = true
 
-func _on_death_area_entered(area):
+func _on_death_area_entered(_area):
 	queue_free()

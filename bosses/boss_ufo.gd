574868aec5f960b20b2473d5027b8c5c3812ff2a
Vynrae <ghaith.elghaddaf@gmail.com>
2024-02-01 17:02:37 +0100

    Boss Attack Component V2

--- bosses/boss_ufo.gd
+++ bosses/boss_ufo.gd
@@ -1,6 +1,8 @@
 extends Boss
 class_name BossUFO
 
+#TODO: Move all of the attacking into the boss attack component
+
 var CAN_ATTACK: bool = false
 
 func _ready():
@@ -15,9 +17,11 @@ func attack():
 	CAN_ATTACK = false
 	$HorizontalAttackLength.start()
 	$OscillationMovementComponent.set_process(false)
+	$BossAttackHorizontal/AnimationPlayer.play("laser_beam")
 	# TODO: Turn the collider on at a frame of the attack
 
 func _on_basic_attack_length_timer_timeout():
+	$BossAttackHorizontal/AnimationPlayer.stop()
 	$HorizontalAttackCooldown.start()
 	$OscillationMovementComponent.set_process(true)
 

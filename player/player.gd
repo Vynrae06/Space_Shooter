e02c0af7ead13e8c131c68441067b371f15d0c95
Vynrae06 <ghaith.elghaddaf@gmail.com>
2024-02-04 22:04:57 +0100

    Music Player & Visual Bug Fix
    Flash Spawn is now set to not visible on Player Ready

--- player/player.gd
+++ player/player.gd
@@ -28,6 +28,7 @@ signal player_died
 func _ready():
 	get_parent().connect("fight_over", disable_player)
 	$ShotSpawnFlash.texture = ResourceLoader.load(shot_spawn_flash_sprite_path)
+	$ShotSpawnFlash.visible = false
 	$PlaneAnimatedSprite.play("straight")
 
 func _process(_delta):

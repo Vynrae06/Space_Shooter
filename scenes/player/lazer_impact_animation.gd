984035beae326aaf2b8ee9e31b761ddd9489b256
Vynrae06 <ghaith.elghaddaf@gmail.com>
2024-02-07 21:44:54 +0100

    Lazer Impact Random Color

--- scenes/player/lazer_impact_animation.gd
+++ scenes/player/lazer_impact_animation.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 func _ready():
 	$AnimatedSprite2D.play("default")
+	modulate = Color(randf_range(0,1), randf_range(0,1), randf_range(0,1))
 
 func _on_animated_sprite_2d_animation_finished():
 	queue_free()

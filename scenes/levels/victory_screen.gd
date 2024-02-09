366f1ecba225405304aee1fb04c2e8b5e36e025d
Vynrae <ghaith.elghaddaf@gmail.com>
2024-02-09 13:14:51 +0100

    Score Screen V1

--- scenes/levels/victory_screen.gd
+++ scenes/levels/victory_screen.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var name_text_edit: LineEdit = $NameTextEdit
 const save_path: String = "user://scores.tres"
+var score_saved: bool = false
 
 func _ready():
 	var win_time_text: String
@@ -10,12 +11,14 @@ func _ready():
 	else:
 		win_time_text = Global.WIN_TIME
 	$WinTimeLabel.text = win_time_text
-		
-	name_text_edit.grab_focus()
+	$NameTextEdit.grab_focus()
 
 func _on_name_text_edit_text_submitted(player_name):
-	save(Global.WIN_TIME, player_name)
-	get_tree().quit()
+	if !score_saved:
+		score_saved = true
+		#save(Global.WIN_TIME, player_name)
+		$NameTextEdit.text = ""
+		# TODO: Say that the score was saved
 
 func save(time, player_name):
 	var save_data = {
@@ -32,3 +35,9 @@ func save(time, player_name):
 	scores.seek_end()
 	scores.store_line(JSON.stringify(save_data))
 	scores.close()
+
+func _on_reset_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level.tscn")
+
+func _on_exit_button_pressed():
+	get_tree().quit()
